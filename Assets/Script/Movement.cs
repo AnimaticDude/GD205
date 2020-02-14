f75cb1736ad428dfbadb6b16bad6575a257581f9
Henzelcole <henzelcole1@gmai.com>
2020-02-13 20:20:41 -0500

    updated project
    has array of hazards, negative effects

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
    public GameObject player;
+    Vector3 initPos;
+    public Transform hazard;
+    public Transform[] bads; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initPos = player.transform.position;
+        //bads = new Transform[9];
     }
 
     // Update is called once per frame
@@ -46,6 +50,18 @@ player.transform.position += new Vector3(0, 0, -1);
         if (Input.GetKeyDown(KeyCode.R))
         {
             player.transform.position = new Vector3(3, 1, 7);
+        }
+        if (hazard.position == player.transform.position)
+        {
+            player.transform.position = initPos;
+        }
+
+        for (int i = 0; i < bads.Length; i++)
+        {
+            if (bads[i].position == player.transform.position)
+            {
+                player.transform.position = initPos;
+            }
         }
     }
 }
