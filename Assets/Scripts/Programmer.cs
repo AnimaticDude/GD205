163060ab30c5f7e83de774826492c8e53b95d7b0
AnimaticDude <60491538+AnimaticDude@users.noreply.github.com>
2020-05-20 19:30:31 -0400

    Update Programmer.cs
    array script

--- Assets/Scripts/Programmer.cs
+++ Assets/Scripts/Programmer.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Programmer : MonoBehaviour
 {
+    public int numOfProgrammers;
+    public GameObject[] Programmers;
+    Pickup pickup;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Programmers = new GameObject[numOfProgrammers];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < numOfProgrammers; i++)
+        {
+            pickup = GetComponent<Pickup>();
+        }
     }
 }
